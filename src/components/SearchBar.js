bffea182384f4d181fda5287ebd812a4af9cd3c1
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-06 18:43:06 +1000

    added prevent default to prevent refershingthe page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: ''};
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div className='search-bar ui segment'>
-        <div className='ui form'>
+        <div onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Video search</label>
-            <input type='text'></input>
+            <input 
+              type='text'
+              value={this.state.term}
+              onChange={this.onInputChange} //this is a event callback function so it will be an array function up there
+            />
           </div>
         </div>
       </div>
