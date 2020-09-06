480817859d7420af468cc0b9b212c4ffd31f3e31
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-07 00:21:33 +1000

    rendering video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 
-const VideoItem = (props) => {
-  return <div>Video item</div>
-}
+const VideoItem = ({ video }) => {
+  console.log(video);
+  return (
+    <div>
+      <img alt="" src={video.snippet.thumbnails.medium.url} />
+      {video.snippet.title}
+    </div>
+  );
+};
 
 export default VideoItem;
