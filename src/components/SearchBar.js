8be2de6db3952dd406e47bc2c568041504c3a067
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-06 10:14:54 +1000

    showing search bar in mainpage

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class SearchBar extends React.components {
+class SearchBar extends React.Component {
   render() {
     return <div>Search bar</div>;
   }
 }
 
-export default SearchBar;
+export default SearchBar; 
