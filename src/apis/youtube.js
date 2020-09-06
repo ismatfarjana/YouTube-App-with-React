dad9c51f6321c4ffcb68e6ee01009c949e1c9815
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-06 22:04:11 +1000

    fetch vidoe from youtube with axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -13,6 +13,7 @@ export default axios.create({
   params: {
     part: 'snippet',
     maxResults: 5,
+    type: 'video',
     key: KEY
   }
 });
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,17 +4,22 @@ class SearchBar extends React.Component {
   state = { term: ''};
 
   onInputChange = event => {
+    // console.log(event.target.value);
     this.setState({ term: event.target.value });
   };
 
   onFormSubmit = event => {
     event.preventDefault();
+
+    //parent component is told what the current state term is
+    console.log(this.state.term);
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className='search-bar ui segment'>
-        <div onSubmit={this.onFormSubmit} className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Video search</label>
             <input 
@@ -23,7 +28,7 @@ class SearchBar extends React.Component {
               onChange={this.onInputChange} //this is a event callback function so it will be an array function up there
             />
           </div>
-        </div>
+        </form>
       </div>
     );
     
