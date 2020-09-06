a93dd0e0fe2bed412e2fd61fda6d4a1b16592194
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-06 19:25:50 +1000

    added youtube api

---
+++ src/apis/youtubes.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const KEY ='AIzaSyCwi-M_OsaKYbZ7VnuxLmveIr5OdOnscFM';
+
+
+
+//setup axiox
+//preconfigured instaneof axiox with default paramete
+
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
